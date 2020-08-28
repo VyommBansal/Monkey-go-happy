b35e3ffb199d8d25df7ebb785ee956f0ee009cb3
VyommBansal <69067057+VyommBansal@users.noreply.github.com>
2020-08-28 15:01:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+
+var monkey;
+var bananaGroup, bananaImage ;
+var stoneGroup, stoneImage ; 
+
+var score = 0;
+
+function preload (){
+  
+  backImage = loadImage ("jungle.png")
+  monkeyImage = loadImage ("Monkey_01.png");
+  bananaImage = loadImage ("banana.png");
+  stoneImage = loadImage ("stone.png");
+  
+
+  
+}
+  
+  function setup() {
+  createCanvas(400, 400);
+    
+    var background 
+    
+}
+
+
+function draw() {
+  background(220);
+}
